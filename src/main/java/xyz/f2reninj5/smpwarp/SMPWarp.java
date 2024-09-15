84f9e765d55be4bd1f85de925305aa35e58f0346
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2024-09-15 23:56:30 +0100

    Figuring out BlueMap implementation

--- src/main/java/xyz/f2reninj5/smpwarp/SMPWarp.java
+++ src/main/java/xyz/f2reninj5/smpwarp/SMPWarp.java
@@ -1,9 +1,14 @@
 package xyz.f2reninj5.smpwarp;
 
+import de.bluecolored.bluemap.api.BlueMapAPI;
+import de.bluecolored.bluemap.api.BlueMapMap;
+import de.bluecolored.bluemap.api.markers.MarkerSet;
+import de.bluecolored.bluemap.api.markers.POIMarker;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.f2reninj5.smpwarp.command.BackCommand;
@@ -11,9 +16,13 @@ import xyz.f2reninj5.smpwarp.command.CreateWarpCommand;
 import xyz.f2reninj5.smpwarp.command.WarpCommand;
 import xyz.f2reninj5.smpwarp.database.WarpDatabase;
 import xyz.f2reninj5.smpwarp.listener.TeleportListener;
+import xyz.f2reninj5.smpwarp.model.Warp;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class SMPWarp extends JavaPlugin {
 
@@ -45,6 +54,32 @@ public final class SMPWarp extends JavaPlugin {
         }
 
         getServer().getPluginManager().registerEvents(new TeleportListener(), this);
+
+        BlueMapAPI.getInstance().ifPresent(api -> {
+            Map<World, List<Warp>> warps = new HashMap<>();
+
+            try {
+                warpDatabase.getAllWarps().forEach(warp -> {
+                    if (!warps.containsKey(warp.location.getWorld())) {
+                        warps.put(warp.location.getWorld(), new ArrayList<>());
+                    }
+                    warps.get(warp.location.getWorld()).add(warp);
+                });
+
+                warps.keySet().forEach(w -> {
+                    api.getWorld(w).ifPresent(world -> {
+                        MarkerSet markerSet = MarkerSet.builder().label("Warps").build();
+                        markerSet.getMarkers().put("id", POIMarker.builder().label(w.getName()).position(0.0, 0.0, 0.0).build());
+
+                        for (BlueMapMap map : world.getMaps()) {
+                            map.getMarkerSets().put("someid", markerSet);
+                        }
+                    });
+                });
+            } catch (SQLException exception) {
+                throw new RuntimeException(exception);
+            }
+        });
 
         LifecycleEventManager<Plugin> manager = this.getLifecycleManager();
         manager.registerEventHandler(LifecycleEvents.COMMANDS, event -> {
