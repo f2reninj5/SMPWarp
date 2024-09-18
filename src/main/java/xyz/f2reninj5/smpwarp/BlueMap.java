7683252296ad51c2c6abc839c0e8d571eb68e86d
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2024-09-18 19:30:29 +0100

    Updated method

--- src/main/java/xyz/f2reninj5/smpwarp/BlueMap.java
+++ src/main/java/xyz/f2reninj5/smpwarp/BlueMap.java
@@ -8,7 +8,6 @@ import org.bukkit.World;
 import xyz.f2reninj5.smpwarp.model.Warp;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -50,34 +49,11 @@ public class BlueMap {
     }
 
     public static void onEnable(BlueMapAPI api) {
-        Map<World, List<Warp>> warps = new HashMap<>();
-
-        try {
-            SMPWarp.getWarpDatabase().getAllWarps().forEach(warp -> {
-                if (!warps.containsKey(warp.location.getWorld())) {
-                    warps.put(warp.location.getWorld(), new ArrayList<>());
-                }
-                warps.get(warp.location.getWorld()).add(warp);
-            });
-
-            warps.keySet().forEach(w -> {
-                api.getWorld(w).ifPresent(world -> {
-                    MarkerSet markerSet = MarkerSet.builder().label("Warps").build();
-//                        markerSet.getMarkers().put("id", POIMarker.builder().label(w.getName()).position(0.0, 0.0, 0.0).build());
-
-                    for (Warp warp : warps.get(w)) {
-                        markerSet.getMarkers().put(warp.group + warp.name,
-                                POIMarker.builder().label(warp.group + warp.name).position(warp.location.getX(),
-                                        warp.location.getY(), warp.location.getZ()).build());
-                    }
-
-                    for (BlueMapMap map : world.getMaps()) {
-                        map.getMarkerSets().put("someid", markerSet);
-                    }
-                });
-            });
-        } catch (SQLException exception) {
-            throw new RuntimeException(exception);
-        }
+        getMarkerSets().forEach((world, markerSet) ->
+            api.getWorld(world).ifPresent(mapWorld -> {
+                for (BlueMapMap map : mapWorld.getMaps()) {
+                    map.getMarkerSets().put("warps", markerSet);
+            }
+        }));
     }
 }
