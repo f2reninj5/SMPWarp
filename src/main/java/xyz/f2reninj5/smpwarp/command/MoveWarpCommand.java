8b7c630ab9f936d1919b9ad3c0fb5f91c0fb8e6f
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2024-09-19 21:03:51 +0100

    Copied warp command

---
+++ src/main/java/xyz/f2reninj5/smpwarp/command/MoveWarpCommand.java
@@ -0,0 +1,118 @@
+package xyz.f2reninj5.smpwarp.command;
+
+import io.papermc.paper.command.brigadier.BasicCommand;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import xyz.f2reninj5.smpwarp.SMPWarp;
+import xyz.f2reninj5.smpwarp.model.Warp;
+
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+
+import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.format.NamedTextColor.GOLD;
+import static net.kyori.adventure.text.format.NamedTextColor.RED;
+import static xyz.f2reninj5.smpwarp.Teleport.teleport;
+
+public class MoveWarpCommand implements BasicCommand {
+
+    private Component getSuccessMessage(String warpGroup, String warpName) {
+        TextComponent.Builder builder = text()
+            .content("Warped to ").color(GOLD);
+
+        if (warpGroup != "") {
+            builder
+                .append(text(warpGroup, RED))
+                .append(text(": ", GOLD));
+        }
+
+        return builder
+            .append(text(warpName, RED))
+            .append(text(".", GOLD))
+            .build();
+    }
+
+    private Component getFailureMessage(String warpGroup, String warpName) {
+        TextComponent.Builder builder = text()
+            .content("Warp ").color(RED);
+
+        if (warpGroup != "") {
+            builder
+                .append(text(warpGroup, GOLD))
+                .append(text(": ", RED));
+        }
+
+        return builder
+            .append(text(warpName, GOLD))
+            .append(text(" not found.", RED))
+            .build();
+    }
+
+    private Component getFailureMessage() {
+        return text()
+            .content("No warp given.")
+            .color(RED)
+            .build();
+    }
+
+    @Override
+    public void execute(@NotNull CommandSourceStack stack, @NotNull String[] args) {
+        if (args.length < 1) {
+            stack.getExecutor().sendMessage(getFailureMessage());
+            return;
+        }
+
+        String group = "";
+        String name = args[0];
+
+        if (args.length > 1) {
+            group = args[0];
+            name = args[1];
+        }
+
+        try {
+            Warp warp = SMPWarp.getWarpDatabase().getWarp(name, group);
+            if (warp == null) {
+                stack.getExecutor().sendMessage(getFailureMessage(group, name));
+                return;
+            }
+
+            teleport((Player) stack.getExecutor(), warp.location);
+            stack.getExecutor().sendMessage(getSuccessMessage(group, name));
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public @NotNull Collection<String> suggest(@NotNull CommandSourceStack stack, @NotNull String[] args) {
+        if (args.length == 0) {
+            try {
+                List<String> suggestions = SMPWarp.getWarpDatabase().getWarpGroups();
+                suggestions.addAll(SMPWarp.getWarpDatabase().getWarpNames("", ""));
+                return suggestions;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (args.length == 1) {
+            try {
+                List<String> suggestions = SMPWarp.getWarpDatabase().getWarpGroups(args[0]);
+                suggestions.addAll(SMPWarp.getWarpDatabase().getWarpNames("", args[0]));
+                return suggestions;
+            } catch (SQLException exception) {
+                throw new RuntimeException(exception.getMessage());
+            }
+        } else if (args.length == 2) {
+            try {
+                return SMPWarp.getWarpDatabase().getWarpNames(args[0], args[1]);
+            } catch (SQLException exception) {
+                throw new RuntimeException(exception);
+            }
+        }
+        return List.of();
+    }
+}
