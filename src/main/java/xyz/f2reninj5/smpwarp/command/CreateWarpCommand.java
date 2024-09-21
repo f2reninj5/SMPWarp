92e4183e6aefe03b1e2c33f6d40b16f237eeac29
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2024-09-21 14:28:32 +0100

    Updated method

--- src/main/java/xyz/f2reninj5/smpwarp/command/CreateWarpCommand.java
+++ src/main/java/xyz/f2reninj5/smpwarp/command/CreateWarpCommand.java
@@ -1,6 +1,5 @@
 package xyz.f2reninj5.smpwarp.command;
 
-import de.bluecolored.bluemap.api.markers.POIMarker;
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import net.kyori.adventure.text.Component;
@@ -10,6 +9,7 @@ import org.jetbrains.annotations.NotNull;
 import xyz.f2reninj5.smpwarp.BlueMap;
 import xyz.f2reninj5.smpwarp.SMPWarp;
 import xyz.f2reninj5.smpwarp.model.Warp;
+import xyz.f2reninj5.smpwarp.model.WarpIdentifier;
 
 import java.sql.SQLException;
 import java.util.Collection;
@@ -18,23 +18,16 @@ import java.util.List;
 import static net.kyori.adventure.text.Component.text;
 import static net.kyori.adventure.text.format.NamedTextColor.GOLD;
 import static net.kyori.adventure.text.format.NamedTextColor.RED;
+import static xyz.f2reninj5.smpwarp.common.CommandResponse.getSuccessSerialiser;
+import static xyz.f2reninj5.smpwarp.common.CommandResponse.identiferToWarpPlaceholder;
 
 public class CreateWarpCommand implements BasicCommand {
 
-    private Component getSuccessMessage(String warpGroup, String warpName) {
-        TextComponent.Builder builder = text()
-            .content("Created warp ").color(GOLD);
-
-        if (warpGroup != "") {
-            builder
-                .append(text(warpGroup, RED))
-                .append(text(": ", GOLD));
-        }
-
-        return builder
-            .append(text(warpName, RED))
-            .append(text(".", GOLD))
-            .build();
+    private Component getSuccessResponse(WarpIdentifier identifier) {
+        return getSuccessSerialiser().deserialize(
+            "<primary>Created warp <warp>.</primary>",
+            identiferToWarpPlaceholder(identifier)
+        );
     }
 
     private Component getFailureMessage(String warpGroup, String warpName) {
@@ -88,7 +81,7 @@ public class CreateWarpCommand implements BasicCommand {
                     BlueMap.addMarker(new Warp(name, group, location, stack.getExecutor().getUniqueId().toString()));
                 }
 
-                stack.getExecutor().sendMessage(getSuccessMessage(group, name));
+                stack.getExecutor().sendMessage(getSuccessResponse(group, name));
             }
         } catch (SQLException exception) {
             exception.printStackTrace();
