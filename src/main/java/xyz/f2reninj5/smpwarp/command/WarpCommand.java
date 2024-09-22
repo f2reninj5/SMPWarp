02b7ee961ac9c12d664a42deed8d11335d287d98
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2024-09-22 15:18:29 +0100

    Removed unused imports

--- src/main/java/xyz/f2reninj5/smpwarp/command/WarpCommand.java
+++ src/main/java/xyz/f2reninj5/smpwarp/command/WarpCommand.java
@@ -3,22 +3,16 @@ package xyz.f2reninj5.smpwarp.command;
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import xyz.f2reninj5.smpwarp.event.TeleportEvent;
 import xyz.f2reninj5.smpwarp.model.Warp;
 import xyz.f2reninj5.smpwarp.SMPWarp;
 import xyz.f2reninj5.smpwarp.model.WarpIdentifier;
 
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-import static net.kyori.adventure.text.Component.text;
-import static net.kyori.adventure.text.format.NamedTextColor.GOLD;
-import static net.kyori.adventure.text.format.NamedTextColor.RED;
 import static xyz.f2reninj5.smpwarp.Teleport.teleport;
 import static xyz.f2reninj5.smpwarp.common.CommandResponse.*;
 
